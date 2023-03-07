e1d739669f4ba0b27bed00c3f51f40560abc9fb8
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2023-03-07 02:57:31 +0100

    setting up database

---
+++ src/data.ts
@@ -0,0 +1,241 @@
+interface datatype{
+    id: number,
+    name: string
+    img: string
+    owner: string
+    link: string
+    price: string
+    desc: string
+}
+export const data: datatype[] = [
+   {
+      id: 1,
+      name: "Owls",
+      img: "https://i.seadn.io/gcs/files/d98ce9834799e8b417ad8cc27b90f33f.png?auto=format&w=256",
+      owner: "REI-N",
+      link: "https://opensea.io/collection/owls-wtf",
+      price: "0.27 ETH",
+      desc: "10'000 owls. Randomly generated on-chain from over 90'000'000 possible combinations",
+   },
+   {
+      id: 2,
+      name: "Bored Ape Yacht Club",
+      img: "https://i.seadn.io/gae/Ju9CkWtV-1Okvf45wo8UctR-M9He2PjILP0oOvxE89AyiPPGtrR3gysu1Zgy0hjd2xKIgjJJtWIc0ybj4Vd7wv8t3pxDGHoJBzDB?auto=format&w=1920",
+      owner: "YugaLabs",
+      link: "https://opensea.io/collection/boredapeyachtclub",
+      price: "72.9 ETH",
+      desc: "The Bored Ape Yacht Club is a collection of 10,000 unique Bored Ape NFTs— unique digital collectibles living on the Ethereum blockchain.",
+   },
+   {
+      id: 3,
+      name: "Mutant Ape Yacht Club",
+      img: "https://i.seadn.io/gae/lHexKRMpw-aoSyB1WdFBff5yfANLReFxHzt1DOj_sg7mS14yARpuvYcUtsyyx-Nkpk6WTcUPFoG53VnLJezYi8hAs0OxNZwlw6Y-dmI?auto=format&w=1920",
+      owner: "YugaLabs",
+      link: "https://opensea.io/collection/mutant-ape-yacht-club",
+      price: "15.45 ETH",
+      desc: "The MUTANT APE YACHT CLUB is a collection of up to 20,000 Mutant Apes that can only be created by exposing an existing Bored Ape to a vial of MUTANT SERUM or by minting a Mutant Ape in the public sale.",
+   },
+   {
+      id: 4,
+      name: "CLONE X - X TAKASHI MURAKAMI",
+      img: "https://i.seadn.io/gae/XN0XuD8Uh3jyRWNtPTFeXJg_ht8m5ofDx6aHklOiy4amhFuWUa0JaR6It49AH8tlnYS386Q0TW_-Lmedn0UET_ko1a3CbJGeu5iHMg?auto=format&w=1920",
+      owner: "RTFKTCLONEXTM",
+      link: "https://opensea.io/collection/clonex",
+      price: "4.57 ETH",
+      desc: "20,000 next-gen Avatars, by RTFKT and Takashi Murakami 🌸 .",
+   },
+   {
+      id: 5,
+      name: "Otherdeed for Otherside",
+      img: "https://i.seadn.io/gae/yIm-M5-BpSDdTEIJRt5D6xphizhIdozXjqSITgK4phWq7MmAU3qE7Nw7POGCiPGyhtJ3ZFP8iJ29TFl-RLcGBWX5qI4-ZcnCPcsY4zI?auto=format&w=1920",
+      owner: "OthersideMeta",
+      link: "https://opensea.io/collection/otherdeed",
+      price: "1.795 ETH",
+      desc: "Otherdeed is the key to claiming land in Otherside. Each have a unique blend of environment and sediment — some with resources, some home to powerful artifacts. And on a very few, a Koda roams",
+   },
+   {
+      id: 6,
+      name: "Azuki",
+      img: "https://i.seadn.io/gae/H8jOCJuQokNqGBpkBN5wk1oZwO7LM8bNnrHCaekV2nKjnCqw6UB5oaH8XyNeBDj6bA_n1mjejzhFQUP3O1NfjFLHr3FOaeHcTOOT?auto=format&w=1920",
+      owner: "TeamAzuki",
+      link: "https://opensea.io/collection/azuki",
+      price: "14.6 ETH",
+      desc: "Azuki starts with a collection of 10,000 avatars that give you membership access to The Garden: a corner of the internet where artists, builders, and web3 enthusiasts meet to create a decentralized future.",
+   },
+   {
+      id: 7,
+      name: "Momoguro: Holoself",
+      img: "https://i.seadn.io/gcs/files/ea3c15935f9142ecb0a8d20398281841.png?auto=format&w=1920",
+      owner: "Holoself-Deployer",
+      link: "https://opensea.io/collection/momoguro-holoself",
+      price: "0.355 ETH",
+      desc: "Discovering Momoguro starts with an 8,888 collection of the Holoself Avatars, the key to exploring the Uno Plane.",
+   },
+   {
+      id: 8,
+      name: "Doodles",
+      img: "https://i.seadn.io/gae/7B0qai02OdHA8P_EOVK672qUliyjQdQDGNrACxs7WnTgZAkJa_wWURnIFKeOh5VTf8cfTqW3wQpozGedaC9mteKphEOtztls02RlWQ?auto=format&w=1920",
+      owner: "Doodles_LLC",
+      link: "https://opensea.io/collection/doodles-official",
+      price: "5.2 ETH",
+      desc: "A community-driven collectibles project featuring art by Burnt Toast. Doodles come in a joyful range of colors, traits and sizes with a collection size of 10,000.",
+   },
+   {
+      id: 9,
+      name: "Createra Genesis Land",
+      img: "https://i.seadn.io/gcs/files/0b6d95dd3b877c69364756151b3c244c.jpg?auto=format&w=1920",
+      owner: "TeamCreatera",
+      link: "https://opensea.io/collection/createra-genesis-land",
+      price: "0.533 ETH",
+      desc: "Createra is dedicated to providing the best platform for game creators to build, experience, and monetize content through a wide range of tools and flexible protocols.",
+   },
+   {
+      id: 10,
+      name: "Mocaverse",
+      img: "https://i.seadn.io/gcs/files/6a0b776c9bb3973d1dd8d399353da9f5.gif?auto=format&w=1920",
+      owner: "Mocaverse",
+      link: "https://opensea.io/collection/mocaverse",
+      price: "1.77 ETH",
+      desc: "Mocaverse is the Animoca Brands membership NFT collection of 8,888 unique beings that thrive in unity: The Mocas.",
+   },
+   {
+      id: 11,
+      name: "Utopia Avatars",
+      img: "https://i.seadn.io/gcs/files/b0b96ad523d7dabe189b8e741490b928.jpg?auto=format&w=1920",
+      owner: "UtopiaAvatars",
+      link: "https://opensea.io/collection/utopiaavatars",
+      price: "0.708 ETH",
+      desc: "Utopia Avatars is a non-fungible token (NFT) collection developed by Utopia, a Web3 ecosystem that aims to revolutionize traditional business operations through the use of blockchain technology",
+   },
+   {
+      id: 12,
+      name: "The Captainz",
+      img: "https://i.seadn.io/gcs/files/c49d2493f2ef4a40a5306fdf1f5c6b43.png?auto=format&w=1920",
+      owner: "9GAG",
+      link: "https://opensea.io/collection/thecaptainz",
+      price: "5.5 ETH",
+      desc: "9,999 Captainz, with their pirate crewz, explore the Broken Seas in search of the legendary treasure known as “Memeland”. Join them in their quests for glory, fortune, love, and of course… memes.",
+   },
+   {
+      id: 13,
+      name: "Alpha Prestige - Fusionist",
+      img: "https://i.seadn.io/gcs/files/88f6c5c0a0d59c2ea88d1218d4c71d98.png?auto=format&w=1920",
+      owner: "Fusion-Corp",
+      link: "https://opensea.io/collection/alpha-prestige-fusionist-official",
+      price: "7.25 ETH",
+      desc: "500 Own-and-Play Exclusive Privileged Symbols. Alpha Prestige grants holder permanent, exclusive access to benefits and privileges in the Fusionist universe. ",
+   },
+   {
+      id: 14,
+      name: "Valhalla",
+      img: "https://i.seadn.io/gcs/files/d7936464d55988206c1b16c6929856f6.jpg?auto=format&w=1920",
+      owner: "StackedStudios",
+      link: "https://opensea.io/collection/valhalla",
+      price: "1.0399 ETH",
+      desc: "Valhalla represents more than a collection of digital avatars. It represents decades of gaming culture: the clutch plays, the ELO grind, the OT wins.",
+   },
+   {
+      id: 15,
+      name: "goblintown.wtf",
+      img: "https://i.seadn.io/gae/cb_wdEAmvry_noTfeuQzhqKpghhZWQ_sEhuGS9swM03UM8QMEVJrndu0ZRdLFgGVqEPeCUzOHGTUllxug9U3xdvt0bES6VFdkRCKPqg?auto=format&w=1920",
+      owner: "kingofthegoblin",
+      link: "https://opensea.io/collection/goblintownwtf",
+      price: "0.4719 ETH",
+      desc: "AAAAAAAUUUUUGGGHHHHH gobblins goblinns GOBLINNNNNNNNns wekm ta goblintown yoo sniksnakr DEJEN RATS oooooh rats are yummmz dis a NEFTEEE O GOBBLINGS on da BLOKCHIN wat? oh. crustybutt da goblinking say GEE EMMM DEDJEN RUTS an queenie saay HLLO SWEATIES ok dats all byeby",
+   },
+   {
+      id: 16,
+      name: "a KID called BEAST",
+      img: "https://i.seadn.io/gcs/files/48e81fed9fa93c77c004f8013c6cf268.png?auto=format&w=1920",
+      owner: "aKIDcalledBEAST",
+      link: "https://opensea.io/collection/akidcalledbeast",
+      price: "1.399 ETH",
+      desc: "a KID called BEAST is a Web3 brand and culture that starts with our digital collectibles — 10,000 unique, truly 3D, and AR-Ready. Each BEAST belongs to 1 of 20 beasthoods. The owner is given access to a sub-community of 500 like-minded individuals.",
+   },
+   {
+      id: 17,
+      name: "The Memes by 6529",
+      img: "https://i.seadn.io/gcs/files/8573c42207ea4d7dc1bb6ed5c0b01243.jpg?auto=format&w=1920",
+      owner: "6529Collections",
+      link: "https://opensea.io/collection/thememes6529",
+      price: "0.32 ETH",
+      desc: "The Memes Collection is focused on the fight for the open metaverse (decentralization, community, self-sovereignty) and spreading this message to many people, many wallets.",
+   },
+   {
+      id: 18,
+      name: "Ordinal Kubz",
+      img: "https://i.seadn.io/gcs/files/f1273e4adc887cec40bb92952ca047d2.gif?auto=format&w=1920",
+      owner: "Keungz",
+      link: "https://opensea.io/collection/ordinal-kubz",
+      price: "0.101 ETH",
+      desc: "Ordinal Kubz are a 10,000 Ordinal NFT collection by Keungz.",
+   },
+   {
+      id: 19,
+      name: "This is nouns",
+      img: "https://i.seadn.io/gcs/files/a6749ad57d276cd2149eb8bf7fad1dee.png?auto=format&w=1920",
+      owner: "0xTranqui",
+      link: "https://opensea.io/collection/this-is-nouns",
+      price: "0.0444 ETH",
+      desc: "WTF is Nouns? That’s the question we sought to answer in this crazy video – packing in more artistic styles than we’ve ever put into a project before, and a sprinkling of humour along the way.",
+   },
+   {
+      id: 20,
+      name: "IROIRO",
+      img: "https://i.seadn.io/gcs/files/259e052f055ca1b75dfee782a87d1a6a.png?auto=format&w=1920",
+      owner: "Iroiro-Team",
+      link: "https://opensea.io/collection/iroiro",
+      price: "0.104 ETH",
+      desc: "IROIRO is a colorful NFT collectible designed by artist sashimi & CyberZ, and IROIRO is uwu Labs Family project. You can choose and get the NFT in any color you like🌈",
+   },
+   {
+      id: 21,
+      name: "Roo Troop",
+      img: "https://i.seadn.io/gae/ACV_EFtAaBzWx14Ui35U3qy3fGx-A7d5-vZHCULMWtO9ZfJD2ctgk8LR6_DCtEcEfCt8hqeJ_oPxmqU3v-h4zMpbHNai5KyW4DMA?auto=format&w=1920",
+      owner: "RooTroop",
+      link: "https://opensea.io/collection/roo-troop",
+      price: "0.174 ETH",
+      desc: "Developing the First On-Chain Job Marketplace",
+   },
+   {
+      id: 22,
+      name: "Editions x Guido",
+      img: "https://i.seadn.io/gae/HrPlG89oureGNlwYc9_hFvpm2OPFbQB3KIMbnNI3mc4bkZAsW9gKrGxDXsiFg0PV8emH6_3OK--zYyhTX_wKUa1VN7T2vt047Zv_RA?auto=format&w=1920",
+      owner: "Guido_D",
+      link: "https://opensea.io/collection/editionsxguido",
+      price: "0.01 ETH",
+      desc: "For the culture. The first photography collection that is a living, evolving and deflationary collection through thoughtful and measured burn and redeem mechanics.",
+   },
+   {
+      id: 23,
+      name: "Parallel Alpha",
+      img: "https://i.seadn.io/gae/Nnp8Pdo6EidK7eBduGnAn_JBvFsYGhNGMJ_fHJ_mzGMN_2Khu5snL5zmiUMcSsIqtANh19KqxXDs0iNq_aYbKC5smO3hiCSw9PlL?auto=format&w=1920",
+      owner: "Parallel",
+      link: "https://opensea.io/collection/parallelalpha",
+      price: "0.0015 ETH",
+      desc: "Sci-fi collectable card game with NFTs.",
+   },
+   {
+      id: 24,
+      name: "MG LAND",
+      img: "https://i.seadn.io/gcs/files/e05d1e09349d7fb36c7970e7ac0e054c.png?auto=format&w=1920",
+      owner: "MetaGameSpace",
+      link: "https://opensea.io/collection/mgland",
+      price: "0.151 ETH",
+      desc: "MGLand is the largest NFT Space on blockchain, everyone can bring their NFT into space to socialize, to play games, to hold NFT exhibitions and to trade freely. Join Alpha Season 3 — NFT Exhibition Event With 1,500,000 $MGL Tokens Rewards In Total. https://www.mg.land/",
+   },
+];
+   
+
+  
+
+
+
+
+
+  
+
+
+
+
+
